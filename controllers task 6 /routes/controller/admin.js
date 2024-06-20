90083d51a4582971e4580013341c2c1a2962be95
sageak <52025563+sageak@users.noreply.github.com>
2024-06-20 17:14:13 +0530

    Delete controllers task 6 /routes directory

--- controllers task 6 /routes/controller/admin.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const routeDir = require('../util/path')
-
-
-const admincontroller = (req, res, next) => {
-    res.sendFile(path.join(routeDir, 'views', 'add-product.html'));
-};
-
-module.exports = admincontroller;
--- controllers task 6 /routes/shop.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const shopcontroller = require('../controllers/shop')
-
-router.get('/', shopcontroller);
-
-module.exports = router;
